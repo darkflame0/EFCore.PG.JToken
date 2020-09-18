ea0b83ecd8d03352cfefd3e4ce6ea99403c51b0b
DarkFlame <darkflame0@sina.com>
2020-09-19 05:42:32 +0800

    3.1.4

--- src/EFCore.PG.JToken/Query/ExpressionTranslators/Internal/NpgsqlJsonPocoMemberTranslatorPlugin.cs
+++ src/EFCore.PG.JToken/Query/ExpressionTranslators/Internal/NpgsqlJsonPocoMemberTranslatorPlugin.cs
@@ -15,11 +15,11 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
 {
     public class NpgsqlJsonPocoMemberTranslatorPlugin : IMemberTranslatorPlugin
     {
-        public NpgsqlJsonPocoMemberTranslatorPlugin(ISqlExpressionFactory sqlExpressionFactory)
+        public NpgsqlJsonPocoMemberTranslatorPlugin(IRelationalTypeMappingSource typeMappingSource , ISqlExpressionFactory sqlExpressionFactory)
         {
             Translators = new IMemberTranslator[]
             {
-                new NpgsqlJsonPocoMemberTranslator((NpgsqlSqlExpressionFactory)sqlExpressionFactory)
+                new NpgsqlJsonPocoMemberTranslator(typeMappingSource,(NpgsqlSqlExpressionFactory)sqlExpressionFactory)
             };
         }
 
@@ -28,13 +28,22 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
 
     internal class NpgsqlJsonPocoMemberTranslator : IMemberTranslator
     {
-        private NpgsqlSqlExpressionFactory _sqlExpressionFactory;
-        private RelationalTypeMapping _stringTypeMapping;
+        private readonly IRelationalTypeMappingSource _typeMappingSource;
+        private readonly NpgsqlSqlExpressionFactory _sqlExpressionFactory;
+        private readonly RelationalTypeMapping _stringTypeMapping;
 
-        public NpgsqlJsonPocoMemberTranslator(NpgsqlSqlExpressionFactory sqlExpressionFactory)
+        static readonly bool[][] TrueArrays =
+{
+            Array.Empty<bool>(),
+            new[] { true },
+            new[] { true, true }
+        };
+
+        public NpgsqlJsonPocoMemberTranslator(IRelationalTypeMappingSource typeMappingSource ,NpgsqlSqlExpressionFactory sqlExpressionFactory)
         {
+            _typeMappingSource = typeMappingSource;
             _sqlExpressionFactory = sqlExpressionFactory;
-            _stringTypeMapping = sqlExpressionFactory.FindMapping(typeof(string));
+            _stringTypeMapping = typeMappingSource.FindMapping(typeof(string));
         }
 
         public SqlExpression? Translate(SqlExpression instance, MemberInfo member, Type returnType)
@@ -46,8 +55,9 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
                     || member.DeclaringType == typeof(Newtonsoft.Json.Linq.JArray) && member.Name == nameof(Newtonsoft.Json.Linq.JArray.Count)))
                 {
                     return _sqlExpressionFactory.Function(
-                        mapping.IsJsonb ? "jsonb_array_length" : "json_array_length",
-                        new[] { instance }, typeof(int));
+                       mapping.IsJsonb ? "jsonb_array_length" : "json_array_length",
+                       new[] { instance },
+                       typeof(int));
                 }
 
                 if (member.DeclaringType.IsGenericType 
@@ -59,13 +69,18 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
                     var keyType = (member as PropertyInfo)!.PropertyType.GetGenericArguments()[0];
                     var sub = _sqlExpressionFactory.ApplyDefaultTypeMapping(_sqlExpressionFactory.Function(
                         mapping.IsJsonb ? "jsonb_object_keys" : "json_object_keys",
-                        new[] { instance }, typeof(string)));
+                        new[] { instance },
+                        typeof(string))
+                        );
 
                     var binary = new SqlCustomBinaryExpression(_sqlExpressionFactory.Fragment("select *"), sub, "from", typeof(string), _stringTypeMapping);
-                    SqlExpression r = _sqlExpressionFactory.Function("ARRAY", new SqlExpression[] { binary }, returnType, new NpgsqlArrayTypeMapping("jsonb", _stringTypeMapping));
+                    SqlExpression r = _sqlExpressionFactory.Function(
+                        "ARRAY",
+                        new SqlExpression[] { binary },
+                        returnType);
                     if (keyType == typeof(int))
                     {
-                        r = _sqlExpressionFactory.Convert(r, typeof(int[]), _sqlExpressionFactory.FindMapping(typeof(int[])));
+                        r = _sqlExpressionFactory.Convert(r, typeof(int[]), _typeMappingSource.FindMapping(typeof(int[])));
                     }
                     return r;
                 }
@@ -74,7 +89,10 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
                 && member.DeclaringType.IsGenericCollection()
                 && member.Name == nameof(ICollection.Count))
             {
-                return _sqlExpressionFactory.Function("cardinality", new[] { instance }, typeof(int?));
+                return _sqlExpressionFactory.Function(
+                    "cardinality",
+                    new[] { instance },
+                    typeof(int?));
             }
             return null;
         }
